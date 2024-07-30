d9392d956e6594afea6d4ca46cc9d705f3d0b19b
Evidentlce <107541743+3Alphaa@users.noreply.github.com>
2024-07-30 23:47:45 +0200

    Update Tycoon Rng Pathfinding.lua

--- Tycoon Rng Pathfinding.lua
+++ Tycoon Rng Pathfinding.lua
@@ -1,6 +1,2 @@
 -- Auto cube walk for Tycoon Rng.
--- Do NOT change the name of the variable under this comment.
-
-_G.debug = false -- set this to true if you want debugs
-
 loadstring(game:HttpGet('https://raw.githubusercontent.com/3Alphaa/RandomScripts/main/Tycoon%20Rng%20Pathfinding%20raw.lua'))()
